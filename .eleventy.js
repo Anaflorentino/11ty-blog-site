e6782650b811f8b2f81ef4e66b311a9b66b5394e
Ana Florentino <abmflorentino@gmail.com>
2024-12-01 19:54:26 -0300

    keeping up to date

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
 
+    eleventyConfig.setTemplateFormats(["md", "html"]);
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets')
 
---
+++ src/blog/post-2.md
@@ -0,0 +1,10 @@
+---
+title: Learning CSS Grid
+author: Kevin Powell
+date: 2024-11-10
+image: /assets/blog/article-2.jpg
+imageAlt: A layout diagram of CSS Grid
+description: Understanding how CSS Grid works...
+---
+
+Content of the post
